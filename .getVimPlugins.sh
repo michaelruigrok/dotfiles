34e8169df66f339691c248333c9e95b659a44f8f
Michael Ruigrok <ruigrok.michael@gmail.com>
2019-05-01 11:04:46 +1000

    group plugins by function, add comments describing each one

--- .getVimPlugins.sh
+++ .getVimPlugins.sh
@@ -13,19 +13,27 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim || \
 wget -O ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 (exit $?) || exit 1
 
+# Remove vim-sleuth
+rm -r ~/.vim/bundle/vim-sleuth/
+
 #get other plugins
 cd ~/.vim/bundle
 
-get_plugin roryokane detectindent
-get_plugin sickill vim-pasta
-get_plugin othree html5.vim
-get_plugin vim-airline vim-airline
-get_plugin easymotion vim-easymotion
-get_plugin tpope vim-surround
-get_plugin tpope vim-repeat
-get_plugin tpope vim-unimpaired
-get_plugin vim-scripts screenplay
-get_plugin vim-scripts paredit # see https://gist.github.com/nblumoe/5450099 for usage notes
-get_plugin mattn emmet-vim
+get_plugin tpope vim-repeat # support to repeat custom mappings
+get_plugin mattn emmet-vim # type in css selectors, out comes fully formed HTML
+get_plugin roryokane detectindent # command to guess the correct indentation settings
+get_plugin tpope vim-dispatch
+
+# UI/Syntax
+get_plugin vim-airline vim-airline # Make the bottom line all pretty
+get_plugin othree html5.vim # HTML5/SVG syntax and omnicompletion
+get_plugin vim-scripts screenplay # For writing scripts, of the film variety
+
+# Motions/Mappings
+get_plugin sickill vim-pasta # p/P paste with appropriate indentation
+get_plugin easymotion vim-easymotion # extend f/F and t/T with an interactive multi-jump select
+get_plugin tpope vim-surround # operate on surrounding brackets/tags
+get_plugin vim-scripts paredit.vim # lisp list maps. See https://gist.github.com/nblumoe/5450099 for usage notes
+get_plugin tpope vim-unimpaired # collection of mappings using ] and [
 
 echo "----------------------------------------------------------------------"
