e11c61778ed430e7a2257353d23c62449b3f8d36
Michael Ruigrok <ruigrok.michael@gmail.com>
2019-10-19 09:50:40 +1000

    upgrade minUnit skeleton to operate with cpp

--- .vim/skeleton/minUnit_test.c
+++ .vim/skeleton/minUnit_test.c
@@ -3,23 +3,27 @@
 
 /* MinUnit source */
 #define mu_assert(message, test) do { if (!(test)) return message; } while (0)
-#define mu_run_test(test) do { char *message = test(); tests_run++; \
+#define mu_run_test(test) do { const char *message = test(); tests_run++; \
 	if (message) return message; } while (0)
 
-int tests_run = 0;
+//Empty function to help with debugging
+static void break_here(void) {
+}
 
-static char* test() {
+static int tests_run = 0;
+
+static const char* test() {
 	mu_assert("Fail Message", true);
 	return 0;
 }
 
-static char* all_tests() {
+static const char* all_tests() {
 	mu_run_test(test);
 	return 0;
 }
 
 int main(int argc, char **argv) {
-	char *result = all_tests();
+	const char *result = all_tests();
 
 	if (result != 0) {
 		printf("%s\n", result);
