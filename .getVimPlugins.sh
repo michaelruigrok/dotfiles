d1d9e3bff50261cf996bde3951102e3f4f8d27ff
Michael Ruigrok <ruigrok.michael@gmail.com>
2020-09-09 16:36:50 +1000

    update vim plugins
    add editorconfig, linediff, coc, vim-jsx-typescript

--- .getVimPlugins.sh
+++ .getVimPlugins.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # get_plugin git-maintainer git-repo
-function get_plugin() {
+get_plugin() {
 	dir=~/.vim/bundle
 	loc="git://github.com/$1/$2.git"
 	git clone "$loc" "$dir/$2" ||
@@ -30,11 +30,13 @@ get_plugin tpope vim-dispatch
 
 # UI/Syntax
 get_plugin rafi awesome-vim-colorschemes # A Whole heap of colourschemes
-#get_plugin vim-airline vim-airline # Make the bottom line all pretty
 get_plugin itchyny lightline.vim # Make the bottom line all pretty
-get_plugin othree html5.vim # HTML5/SVG syntax and omnicompletion
 get_plugin ap vim-css-color # preview the colours of CSS with highlights
 get_plugin vim-scripts screenplay # For writing scripts, of the film variety
+get_plugin editorconfig editorconfig-vim
+
+# Utilities
+get_plugin AndrewRadev linediff.vim # Diff only portions of a file
 
 # Motions/Mappings
 get_plugin sickill vim-pasta # p/P paste with appropriate indentation
@@ -43,5 +45,11 @@ get_plugin tpope vim-surround # operate on surrounding brackets/tags
 get_plugin tpope vim-commentary # `gc` verb un/comments
 get_plugin vim-scripts paredit.vim # lisp list maps. See https://gist.github.com/nblumoe/5450099 for usage notes
 get_plugin tpope vim-unimpaired # collection of mappings using ] and [
+
+# Language support 
+get_plugin neoclide coc.nvim # Language server support
+get_plugin othree html5.vim
+get_plugin keith swift.vim
+get_plugin peitalin vim-jsx-typescript
 
 echo "----------------------------------------------------------------------"
