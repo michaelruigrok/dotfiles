7f11e1d55c65d1160861dda797ad2a36a2975a9d
Michael Ruigrok <ruigrok.michael@gmail.com>
2025-01-31 14:14:44 +1000

    repl_save: optionally handle vars, repl_save_var is now just an alias

--- .local/lib/shell/repl.sh
+++ .local/lib/shell/repl.sh
@@ -3,22 +3,27 @@ REPL_DIR="/tmp/$USER/repl/"
 repl_start() {
 	mkdir -p "$REPL_DIR"
 	REPL_ID="${1:-$(ls "$REPL_DIR" | wc -l)}"
-	REPL_FILE="$REPL_DIR/$REPL_ID.sh"
+	export REPL_FILE="$REPL_DIR/$REPL_ID.sh"
 }
 
 alias repl_last="history -p '!!'"
 
 repl_save() {
-	repl_last | tee -a "$REPL_FILE"
-}
+	if [ $# -ne 1 ]; then
+		repl_last | tee -a "$REPL_FILE"
+		return
+	fi
 
-repl_save_var() {
-	[ $# -ne 1 ] && { echo >&2 "Usage: repl_save_var VARIABLE_NAME"; return 1; }
 	local var
 	var="$(repl_last | sed 's/.*/'$1'="$(&)"/')"
 	echo "$var" | tee -a "$REPL_FILE"
 	eval "$var"
 	history -s "$var"
+}
+
+repl_save_var() {
+	[ $# -ne 1 ] && { echo >&2 "Usage: repl_save_var VARIABLE_NAME"; return 1; }
+	repl_save "$1"
 }
 
 repl_run() {
